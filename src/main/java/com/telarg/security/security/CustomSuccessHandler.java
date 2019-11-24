3a9c8146e828a7706b1c44925bd8205eff6bcfdf
giardinno <infected_giardinno@hotmail.com>
2019-11-24 14:23:01 -0600

    update: prueba

--- src/main/java/com/telarg/security/security/CustomSuccessHandler.java
+++ src/main/java/com/telarg/security/security/CustomSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.telarg.security.security;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -7,8 +8,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.telarg.security.data.entities.User;
 import com.telarg.security.data.models.TokenDTO;
 import com.telarg.security.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,15 +36,19 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 		TokenDTO tokenDTO = new TokenDTO();
 		tokenDTO.setIdToken(token);
 
-		try ( InputStream is = request.getInputStream() ) {
-			is.reset();
-			DocumentContext context = JsonPath.parse(is);
-			String username = context.read("$.username", String.class);
-			tokenDTO.setUser( userRepository.findByName(username).get() );
+
+		try {
+			BufferedReader reader = request.getReader();
+			Gson gson = new Gson();
+			User user = gson.fromJson(reader, User.class);
+			System.out.println( user.getName() );
+			System.out.println( user.getPassword() );
+			tokenDTO.setUser(userRepository.findByName(user.getName()).get());
 			tokenDTO.getUser().setPassword(null);
-		} catch (Exception e) {
-			e.printStackTrace();
+		}catch (Exception e){
+			System.out.println(e);
 		}
+
 		response.addHeader("Content-Type", "application/json");
 		response.getWriter().write( new ObjectMapper().writeValueAsString( tokenDTO ) );
 		response.setStatus( HttpServletResponse.SC_OK );
