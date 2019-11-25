8bb7da731ed3e0c62bf488c2362161933ba2e65d
giardinno <infected_giardinno@hotmail.com>
2019-11-24 22:37:38 -0600

    Update: se añade repo para borrar

--- src/main/java/com/telarg/security/controllers/DesconocidosController.java
+++ src/main/java/com/telarg/security/controllers/DesconocidosController.java
@@ -9,10 +9,7 @@ import com.telarg.security.utils.Classifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DesconocidosController {
@@ -28,7 +25,7 @@ public class DesconocidosController {
         return new ResponseEntity<>(mensajesDesconocidosRepository.findAll(), HttpStatus.OK);
     }
 
-    @DeleteMapping("/desconocidos/{id}")
+    @PostMapping("/desconocidos/{id}")
     public ResponseEntity<Object> saveDesconocido(@PathVariable("id") int id){
         MensajesDesconocidos  mensajesDesconocidos = mensajesDesconocidosRepository.findById(id).get();
         historicoRepository.save(new Historico(new Clasificacion(Classifications.RECONOCIMIENTO), mensajesDesconocidos.getMessage()));
