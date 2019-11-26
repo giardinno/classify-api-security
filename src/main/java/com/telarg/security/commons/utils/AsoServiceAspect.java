1b2794db9ef43e9e0255796b703aa226930a1fbd
giardinno <infected_giardinno@hotmail.com>
2019-11-26 17:13:51 -0600

    Update: prueba2

--- src/main/java/com/telarg/security/commons/utils/AsoServiceAspect.java
+++ src/main/java/com/telarg/security/commons/utils/AsoServiceAspect.java
@@ -7,8 +7,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -26,7 +24,7 @@ public class AsoServiceAspect {
     LoggerMetrics loggerMetrics;
 
     @Around("execution(* com.telarg.security.services.fiengClients.*.*(..))")
-    public ClassifyResponse asoServiceExecution(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    public Object asoServiceExecution(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
                 .currentRequestAttributes())
                 .getRequest();
