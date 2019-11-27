22cd644c2709725f5f49f2ad2bca565e256d8c49
giardinno <infected_giardinno@hotmail.com>
2019-11-27 07:54:41 -0600

    update: añade valid

--- src/main/java/com/telarg/security/data/vo/ClassifyRequest.java
+++ src/main/java/com/telarg/security/data/vo/ClassifyRequest.java
@@ -1,10 +1,12 @@
 package com.telarg.security.data.vo;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class ClassifyRequest {
 
     @Size(max = 150)
+    @NotEmpty
     private String message;
 
     public String getMessage() {
